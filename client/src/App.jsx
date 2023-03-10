fc418ebd8e24a777cbd5d965874171cb2931e224
Noah Fajarda <noahfajarda@gmail.com>
2023-03-09 19:34:53 -0800

    added secondary login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 // React Boilerplate
-// import React, { useEffect, useState } from "react";
-import React from "react";
+import React, { useEffect, useState } from "react";
 // import "./App.css";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
@@ -9,6 +8,9 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import TestPage from "./pages/TestPage";
 import HomePage from "./pages/HomePage";
+import QuestionPage from "./pages/QuestionPage";
+import LoginPage from "./pages/LoginPage";
+import LoginPage1 from "./pages/LoginPage1";
 
 const client = new ApolloClient({
   uri: "/graphql",
@@ -27,6 +29,10 @@ function App() {
               <Route path="/test" element={<TestPage />} />
               {/* all other routes */}
               <Route path="/home" element={<HomePage />} />
+              <Route path="/question" element={<QuestionPage />} />
+              <Route path="/login" element={<LoginPage1 />} />
+              {/* <Route path="/login" element={<LoginPage />} /> */}
+              {/* all other routes */}
               <Route path="*" element={<MainPage />} />
             </Routes>
           </header>
