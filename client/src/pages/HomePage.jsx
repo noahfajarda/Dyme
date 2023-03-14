467b02ffc89365eb50d9154f2e17d0f39d2a3de4
Anas Qazza <anasqazza@gmail.com>
2023-03-13 19:14:05 -0700

    minor css changes

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -159,34 +159,6 @@ function HomePage() {
           <div className="chart-container-wrapper">
             <div className="chart-container">
               <div className="chart-info-wrapper">
-                <h2>Budget</h2>
-                <span>5.5 K</span>
-              </div>
-              <div className="chart-svg">
-                <svg viewBox="0 0 36 36" className="circular-chart blue">
-                  <path
-                    className="circle-bg"
-                    d="M18 2.0845
-          a 15.9155 15.9155 0 0 1 0 31.831
-          a 15.9155 15.9155 0 0 1 0 -31.831"
-                  ></path>
-                  <path
-                    className="circle"
-                    stroke-dasharray="60, 100"
-                    d="M18 2.0845
-          a 15.9155 15.9155 0 0 1 0 31.831
-          a 15.9155 15.9155 0 0 1 0 -31.831"
-                  ></path>
-                  <text x="18" y="20.35" className="percentage">
-                    60%
-                  </text>
-                </svg>
-              </div>
-            </div>
-          </div>
-          <div className="chart-container-wrapper">
-            <div className="chart-container">
-              <div className="chart-info-wrapper">
                 <h2>Income</h2>
               </div>
               <div id="root">
@@ -235,69 +207,20 @@ function HomePage() {
                         </span>
                       </div>
                     </div>
-                    {/* <div class="c-dashboardInfo col-lg-3 col-md-6">
-                      <div class="wrap">
-                        <h4 class="heading heading5 hind-font medium-font-weight c-dashboardInfo__title">
-                          Available funds
-                          <svg
-                            class="MuiSvgIcon-root-19"
-                            focusable="false"
-                            viewBox="0 0 24 24"
-                            aria-hidden="true"
-                            role="presentation"
-                          >
-                            <path fill="none" d="M0 0h24v24H0z"></path>
-                            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z"></path>
-                          </svg>
-                        </h4>
-                        <span class="hind-font caption-12 c-dashboardInfo__count">
-                          €5000
-                        </span>
-                      </div>
-                    </div> */}
-                    {/* <div class="c-dashboardInfo col-lg-3 col-md-6"> */}
-                    {/* <div class="wrap">
-                        <h4 class="heading heading5 hind-font medium-font-weight c-dashboardInfo__title">
-                          Rental return
-                          <svg
-                            class="MuiSvgIcon-root-19"
-                            focusable="false"
-                            viewBox="0 0 24 24"
-                            aria-hidden="true"
-                            role="presentation"
-                          >
-                            <path fill="none" d="M0 0h24v24H0z"></path>
-                            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z"></path>
-                          </svg>
-                        </h4>
-                        <span class="hind-font caption-12 c-dashboardInfo__count">
-                          6,40%
-                        </span>
-                      </div> */}
-                    {/* </div> */}
                   </div>
                 </div>
               </div>
-              {/* <div className="chart-svg">
-                <svg viewBox="0 0 36 36" className="circular-chart orange">
-                  <path
-                    className="circle-bg"
-                    d="M18 2.0845
-          a 15.9155 15.9155 0 0 1 0 31.831
-          a 15.9155 15.9155 0 0 1 0 -31.831"
-                  ></path>
-                  <path
-                    className="circle"
-                    stroke-dasharray="90, 100"
-                    d="M18 2.0845
-          a 15.9155 15.9155 0 0 1 0 31.831
-          a 15.9155 15.9155 0 0 1 0 -31.831"
-                  ></path>
-                  <text x="18" y="20.35" className="percentage">
-                    90%
-                  </text>
-                </svg>
-              </div> */}
+            </div>
+          </div>
+          <div className="chart-container-wrapper">
+            <div className="chart-container">
+              <div className="chart-info-wrapper">
+                <h2>Recent Transactions</h2>
+                <h3> 1. Tesco £122.50 05/02/2016</h3>
+                <h3> 2. Game £22.50 05/02/2016 </h3>
+                <h3> 3. Tesco £10.00 01/02/2016 </h3>
+                <h3> 4. Steam £19.99 05/02/2016</h3>
+              </div>
             </div>
           </div>
         </div>
