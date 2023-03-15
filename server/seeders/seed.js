1a925e303945a40e7a3419a8c7145f655819a48c
Noah Fajarda <noahfajarda@gmail.com>
2023-03-15 09:49:46 -0700

    seed script works, just need to edit fields for category in expenses

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,15 +1,31 @@
 const db = require('../config/connection');
 // insert user & Catogory models
-const { Profile } = require('../models');
+const { User, Expense } = require('../models');
 // insert user & Catogory seeds
-const profileSeeds = require('./profileSeeds.json');
+const userSeeds = require('./userSeeds.json');
+const expenseSeeds = require('./expenseSeeds.json');
 
 db.once('open', async () => {
   try {
-    // delete existing users & cateogries
-    await Profile.deleteMany({});
+    // delete existing users & expenses
+    await User.deleteMany({});
+    await Expense.deleteMany({});
     // create existing users & categories
-    await Profile.create(profileSeeds);
+    await User.create(userSeeds);
+    // await Expense.create(expenseSeeds);
+
+
+    for (let i = 0; i < expenseSeeds.length; i++) {
+      const { _id, associatedUser } = await Expense.create(expenseSeeds[i]);
+      const user = await User.findOneAndUpdate(
+        { username: associatedUser },
+        {
+          $addToSet: {
+            expenses: _id,
+          },
+        }
+      );
+    }
 
     console.log('all done!');
     process.exit(0);
