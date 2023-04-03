dc366ba5ffe1eb1e4ddb2b811089e5528ffac94a
amoubasher <amoubasher@gmail.com>
2023-04-03 15:14:37 -0700

    heroku test

--- server/server.js
+++ server/server.js
@@ -17,11 +17,11 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
 
-app.get('/', (req, res) => {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+
+
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 // app.get("/api", (req, res) => {
